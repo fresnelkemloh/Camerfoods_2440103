a568d2b04c71e9a25176121702d1e9060af7c1c3
Fresnel Kemloh <fresnelkemloh@gmail.com>
2025-11-13 11:42:50 -0500

    ajout des elements pour la navigation avec les strings pour home et parametre

--- app/src/main/java/com/example/camerfoods/MainActivity.kt
+++ app/src/main/java/com/example/camerfoods/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.camerfoods
 
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,6 +25,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontVariation
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -28,6 +34,27 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+
+sealed class Screen(
+    val route: String,
+    val titleResource: Int,
+    val icon: ImageVector
+) {
+    object  Home : Screen(
+        route = "home",
+        titleResource = R.string.home,
+        icon = Icons.Default.Home
+    )
+    object Settings : Screen(
+        route = "settings",
+        titleResource = R.string.home,
+        icon =  Icons.Default.Settings
+    )
+
+    companion object{
+        val items = listOf(Home,Settings)
+    }
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
