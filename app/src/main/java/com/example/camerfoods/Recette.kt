861f7d3b1739d453b50f556c82f8c910f1b48184
Fresnel Kemloh <fresnelkemloh@gmail.com>
2025-12-03 00:31:38 -0500

    ajout des donnees pour implementer les recettes

---
+++ app/src/main/java/com/example/camerfoods/Recette.kt
@@ -0,0 +1,12 @@
+package com.example.camerfoods
+
+data class Recette(
+    val id: String,
+    val titre: String,
+    val temps: String,
+    val description: String,
+    val ingredients: List<String>,
+    val instructions: List<String>,
+    val image: String,
+    var favoris: Boolean= false
+)
