96764dd1bec9c5c5fa1d9d2b37f911bca6c09d42
Fresnel Kemloh <fresnelkemloh@gmail.com>
2025-12-06 19:50:25 -0500

    ajout du titre au centre et d'une recette

--- app/src/main/java/com/example/camerfoods/MainActivity.kt
+++ app/src/main/java/com/example/camerfoods/MainActivity.kt
@@ -30,6 +30,7 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -405,7 +406,7 @@ fun AppNavigation(
 
     Scaffold(
         topBar = {
-            TopAppBar(
+            CenterAlignedTopAppBar(
                 title = { Text("CamerFoods")}
             )
         },
